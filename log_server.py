48cf1eb975ef76913a168d75404b40c114423874
Xtinc <remingtonscarlet@outlook.com>
2024-02-04 21:08:06 +0800

    simple log server

---
+++ log_server.py
@@ -0,0 +1,22 @@
+import socketserver
+
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        len_data = 1
+        while len_data != 0:
+            data = self.rfile.readline().strip()
+            len_data = len(data)
+            print(data.decode())
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
